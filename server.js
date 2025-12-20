12f4e36e8ba5ddb7e8b312e1d23eb9fdfe424d32
ztech-sys <244317230+ztech-sys@users.noreply.github.com>
2025-12-20 10:10:40 +0700

    Update server.js
    Signed-off-by: ztech-sys <244317230+ztech-sys@users.noreply.github.com>

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ console.log("Telegram Mini-App bot running");
 
 // Tạo server HTTP dummy để Render coi là web service
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
   res.send("Mini-App bot is running ✅");
